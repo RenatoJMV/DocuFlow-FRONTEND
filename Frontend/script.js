651d4ea9f81b20ba2eead3e56fa45206ad09a9eb
Renato Muñoz Villafana <145702251+Holapex9@users.noreply.github.com>
2025-08-25 04:51:25 -0500

    FrontEnd

---
+++ Frontend/script.js
@@ -0,0 +1,30 @@
+// script.js (para login)
+
+document.getElementById('loginForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        const response = await fetch('http://localhost:8080/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password })
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            localStorage.setItem('token', data.token); // Guardamos el JWT
+
+            // Redirigir a upload.html
+            window.location.href = 'upload.html';
+        } else {
+            document.getElementById('error-message').textContent = 'Credenciales incorrectas';
+            document.getElementById('error-message').style.display = 'block';
+        }
+    } catch (error) {
+        console.error("Error en la conexión:", error);
+        alert("No se pudo conectar con el servidor.");
+    }
+});
