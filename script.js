a0e238ff00c2404fb5fe77ca89714afa31669010
Renato Muñoz Muñoz <145702251+Holapex9@users.noreply.github.com>
2025-08-25 05:18:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById('loginForm').addEventListener('submit', async function(e
     const password = document.getElementById('password').value;
 
     try {
-        const response = await fetch('http://localhost:8080/login', {
+        const response = await fetch('https://touched-included-elephant.ngrok-free.app/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password })
